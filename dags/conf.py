1f37fe238d591dc0a6c387d361323df56bfd8b7b
joosep <joosephook@gmail.com>
2022-11-03 18:16:44 +0200

    stuff

---
+++ dags/conf.py
@@ -0,0 +1,14 @@
+DEFAULT_ARGS = {
+    'owner': 'Project',
+    'depends_on_past': False,
+    'retries': 0,
+}
+API_URL = 'http://65.108.50.112:5000/data/arxiv'
+API_PARAMS = {
+    'results': 50,
+    'format': 'csv',
+    'inc': 'title,doi'
+}
+DATA_FOLDER = '/tmp/data'
+ARXIV_FILE_NAME = 'arxiv.csv'
+SQL_FILE_NAME = 'insert_arxiv.sql'

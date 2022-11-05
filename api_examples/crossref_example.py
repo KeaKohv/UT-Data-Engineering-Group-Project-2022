504dcb722465885ef9e4213089be3c623d1a4eab
joosep <afds@a>
2022-11-05 13:33:46 +0200

    Extract author from authors_parsed as it may contain affiliation

--- api_examples/crossref_example.py
+++ api_examples/crossref_example.py
@@ -1,4 +1,5 @@
 from habanero import Crossref
+from pprint import pprint
 import json
 
 # example record without doi, journal, publishing date
@@ -15,10 +16,41 @@ cr = Crossref()
 response = cr.works( limit=1, **query)
 
 fields = ['DOI', 'title', 'author', 'type', 'publisher', 'is-referenced-by-count', 'container-title', 'subject', 'published']
-from operator import itemgetter
 def extract(item, fields):
-    return dict(zip(fields, itemgetter(*fields)(item)))
+    return {
+        field:item.get(field)
+        for field in fields
+    }
+
+def authors(parsed):
+    """
+    Extract authors from authors_parsed field, ignoring affiliations
+    :param parsed:
+    :return:
+    """
+    authors = []
+    for p in parsed:
+        author = []
+        for t in p:
+            if len(t) > 0:
+                author.append(t)
+            else:
+                break
+        authors.append(author)
+    return authors
 
 aux = extract(response['message']['items'][0], fields)
-from pprint import pprint
 pprint(aux)
+
+line = r"""{"id":"hep-th/9608091","submitter":"Francesco Ravanini","authors":"D. Fioravanti, A. Mariottini, E. Quattrini and F. Ravanini (Bologna\n  Univ. and INFN)","title":"Excited State Destri - De Vega Equation for Sine-Gordon and Restricted\n  Sine-Gordon Models","comments":"Latex, 12 pages","journal-ref":"Phys.Lett.B390:243-251,1997","doi":"10.1016/S0370-2693(96)01409-8","report-no":"DFUB 96-24","categories":"hep-th","license":null,"abstract":"  We derive a generalization of the Destri - De Vega equation governing the\nscaling functions of some excited states in the Sine-Gordon theory. In\nparticular configurations with an even number of holes and no strings are\nanalyzed and their UV limits found to match some of the conformal dimensions of\nthe corresponding compactified massless free boson. Quantum group reduction\nallows to interpret some of our results as scaling functions of excited states\nof Restricted Sine-Gordon theory, i.e. minimal models perturbed by phi_13 in\ntheir massive regime. In particular we are able to reconstruct the scaling\nfunctions of the off-critical deformations of all the scalar primary states on\nthe diagonal of the Kac-table.\n","versions":[{"version":"v1","created":"Wed, 14 Aug 1996 16:31:26 GMT"}],"update_date":"2010-11-19","authors_parsed":[["Fioravanti","D.","","Bologna\n  Univ. and INFN"],["Mariottini","A.","","Bologna\n  Univ. and INFN"],["Quattrini","E.","","Bologna\n  Univ. and INFN"],["Ravanini","F.","","Bologna\n  Univ. and INFN"]]}"""
+record = json.loads(line)
+query = dict(
+    query_author=authors(record['authors_parsed']),
+    doi=record['doi'],
+    query_title=record['title'],
+)
+response = cr.works( limit=1, **query)
+pprint(response['message']['items'])
+aux = extract(response['message']['items'][0], fields)
+pprint(aux)
+
